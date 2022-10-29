9ab9048d031572190ea82f55895af1c2599a1b74
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-10-29 20:11:59 -0300

    ♻ refactor: novo header com menu mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+} from "@angular/core";
+
+import { fromEvent, Subscription } from "rxjs";
+import { filter, throttleTime } from "rxjs/operators";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnDestroy, OnInit {
+  constructor(private elementRef: ElementRef) {}
 
-  constructor() { }
+  isOpen = false;
+  resize: Subscription;
 
-  ngOnInit() {
+  @HostListener("document:click", ["$event"]) onClick(event) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.checkIfNavDropDown();
+    }
   }
 
+  ngOnDestroy() {
+    this.resize.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.resize = fromEvent(window, "resize")
+      .pipe(
+        throttleTime(500),
+        filter(() => !!this.elementRef)
+      )
+      .subscribe(() => this.checkIfNavDropDown());
+  }
+
+  checkIfNavDropDown() {
+    if (this.isOpen) {
+      this.isOpen = false;
+    }
+  }
+
+  onMenu() {
+    this.isOpen = !this.isOpen;
+  }
 }
