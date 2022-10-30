19a821d43af9cf668e9ff784ee807afc028dc867
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-10-30 13:47:28 -0300

    ✨ feat: add paginação no componente card-post

--- src/app/components/card-post/card-post.component.ts
+++ src/app/components/card-post/card-post.component.ts
@@ -10,11 +10,23 @@ import { PlaceholderApiService } from "src/app/services/placeholder-api.service"
 export class CardPostComponent implements OnInit {
   constructor(private postsApi: PlaceholderApiService) {}
 
+  startIndex = 0;
+  endIndex = 10;
+  itemsPerPage = 10;
   posts: IPost[];
 
   ngOnInit() {
     this.postsApi.getAllPosts().subscribe((result: IPost[]) => {
       this.posts = result;
     });
+  }
+
+  getArrayLenght(length: number) {
+    return new Array(length / this.itemsPerPage);
+  }
+
+  getIndex(pageIndex: number) {
+    this.startIndex = pageIndex * this.itemsPerPage;
+    this.endIndex = this.startIndex + this.itemsPerPage;
   }
 }
